0e92c777092d6164a7025dfd6719d3596f28cbd5
Büşra Semiz <75666551+busrasemiiz@users.noreply.github.com>
2020-12-23 16:26:10 +0300

    Update paracek_parayatir.py

--- paracek_parayatir.py
+++ paracek_parayatir.py
@@ -1,19 +1,12 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Dec 23 16:18:13 2020
-
-@author: Büşra Semiz
-"""
-
 class Account:
     def __init__(self,isim,numara,bakiye):
         self.isim = isim
         self.numara = numara
         self.bakiye = bakiye
-    def hesapbilgileri(self):   #şimdi ben bu classıma yani her bir objeme birer tane metod vermek istiyorum hesap bilg...yazılır.
+    def hesapbilgileri(self):  
         print("isim:", self.isim)
         print("numara:",self.numara)
-        print("bakiye:",self.bakiye) #bu sayede 3 özelliğimizi bir metod yardımıyla ekrana yazdırmış olacağız.
+        print("bakiye:",self.bakiye) 
     def paracek(self,miktar):
            
         if (self.bakiye-miktar<0):
